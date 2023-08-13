dd8671c2b817d491467f83df579e2c2d38abf1fe
michele25-10 <michele.gabri01@gmail.com>
2023-08-13 10:42:56 +0200

    file .env, server.js, errorHandler, constants

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const errorHandler = require("./middleware/errorHandler");
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000; //prendo la porta dal file .env
+//const connectDb = require("./config/dbConnection");
+
+//connectDb(); //connessione al database
+
+//middleware
+app.use(express.json());
+
+//Aggiunto il middleware
+//app.use("/api/contacts", require("./routes/contactRoutes"))
+//app.use("/api/users", require("./routes/userRoutes"))
+
+//middleware degli errori
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log('Server running on port ' + port);
+}); 
