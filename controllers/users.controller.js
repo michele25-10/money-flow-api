b007a4b401aa938c8722a0069d5b964d276d8d48
michele25-10 <michele.gabri01@gmail.com>
2023-08-14 17:51:04 +0200

    Update users.controller.js

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -9,32 +9,40 @@ const conn = require('../config/dbConnection');
 //@access public
 loginUser = asyncHandler(async (req, res) => {
     const { username, password } = req.body;
+
+    if (!username || !password) {
+        res.status(400);
+        throw new Error("Tutti i campi sono obbligatori");
+    }
+
     const mysql = "SELECT id, password, username, auth, level FROM `user` WHERE username = '" + username + "' AND status='0'; ";
-    await conn.query(mysql, function (err, rows, fields) {
-        conn.end();
+
+    await conn.query(mysql, function (err, rows) {
         if (err) {
             res.status(500);
             throw new Error(err);
         }
         if (rows.length == 1) {
-            if (verifyPassword(rows[0].password, password) == true) {
-                conn.end();
-                const accessToken = jwt.sign({
-                    /*Payload incorporato all'interno del token */
-                    user: {
-                        id: rows[0].id,
-                        username: rows[0].username,
-                        auth: rows[0].auth,
-                        level: rows[0].level
-                    }
-                },
-                    process.env.ACCESS_TOKEN_SECRET,
-                    { expiresIn: "1h" });
-                res.status(200).json({ accessToken });
-            } else {
-                res.status(400);
-                throw new Error("Password o email erratta");
-            };
+            bcrypt.compare(password, rows[0].password, function (err, result) {
+                console.log(result);
+                if (result == true) {
+                    const accessToken = jwt.sign({
+                        /*Payload incorporato all'interno del token */
+                        user: {
+                            id: rows[0].id,
+                            username: rows[0].username,
+                            auth: rows[0].auth,
+                            level: rows[0].level
+                        }
+                    },
+                        process.env.ACCESS_TOKEN_SECRET,
+                        { expiresIn: "1h" });
+                    res.status(200).json({ accessToken });
+                } else {
+                    res.status(400);
+                    throw new Error("Password o email erratta");
+                };
+            });
         } else {
             res.status(500);
             throw new Error("Internal error");
@@ -43,11 +51,3 @@ loginUser = asyncHandler(async (req, res) => {
 });
 
 module.exports = { loginUser };
-
-function verifyPassword(passwordQuery, password) {
-    if (bcrypt.compare(password, passwordQuery)) {
-        return true;
-    } else {
-        return false;
-    }
-}
