3e902912a7d376770f34fc593a83d2ef898f136a
michele25-10 <michele.gabri01@gmail.com>
2023-11-19 18:33:01 +0100

    Nuovo script db

--- config/db.sql
+++ config/db.sql
@@ -2,6 +2,17 @@ create database moneyFlow;
 
 use moneyFlow; 
 
+create table log(
+    id int unsigned not null auto_increment primary key, 
+    ip_address varchar(15), 
+    status bit not null
+);
+
+create table famiglia(
+    id INT UNSIGNED NOT NULL   AUTO_INCREMENT  PRIMARY key,
+    cognome varchar(30) not null, 
+)
+
 create table `user` (
 id INT UNSIGNED NOT NULL   AUTO_INCREMENT  PRIMARY key,
 nome varchar(30) not null, 
@@ -9,47 +20,30 @@ cognome varchar(30) not null,
 username varchar(30) not null, 
 password varchar(64) not null, 
 auth int not null,
-level int not null
+level int not null,
+id_famiglia int unsigned not null,
+FOREIGN KEY (id_famiglia)
+    REFERENCES famiglia(id)
 );
 
 create table spesa(
 id INT UNSIGNED NOT NULL   AUTO_INCREMENT  PRIMARY key,
 id_utente int unsigned not null, 
-luogo varchar(50),
+luogo varchar(100),
 data date not null, 
 `desc` varchar(100),
 importo double not null, 
-status int default(0) not null,
 tipo_pagamento int not null,
 id_categoria int unsigned not null,
-proprietario_soldi int unsigned not null
+proprietario_soldi int unsigned not null,
+documento BLOB, 
+FOREIGN KEY (id_utente)
+    REFERENCES `user`(id),
+FOREIGN KEY (id_categoria)
+    REFERENCES categoria(id)
 );
 
 create table categoria(
 id INT UNSIGNED NOT NULL   AUTO_INCREMENT  PRIMARY key,
 nome nvarchar(30) not null
 );
-
-create table reddito(
-id INT UNSIGNED NOT NULL   AUTO_INCREMENT  PRIMARY key,
-data date not null, 
-somma double not null, 
-provenienza varchar(30) not null,
-status int default(0) not null,
-id_utente int unsigned not null
-);
-
-create table banca(
-id INT UNSIGNED NOT NULL   AUTO_INCREMENT  PRIMARY key,
-data date not null, 
-operazione varchar(30),
-importo double not null, 
-status int default(0) not null,
-id_utente int unsigned not null
-);
-
-
-alter table spesa add constraint fk_spesa_categoria foreign key (id_categoria) references categoria (id);
-alter table spesa add constraint fk_spesa_user foreign key (id_utente) references `user`(id);
-alter table reddito add constraint fk_reddito_user foreign key (id_utente) references `user`(id);
-alter table banca add constraint fk_banca_user foreign key (id_utente) references `user`(id);
