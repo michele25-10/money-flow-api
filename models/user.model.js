7060f7d8edddafcba64d8146ad952f90aeea99e4
michele25-10 <michele.gabri01@gmail.com>
2023-11-20 14:55:37 +0100

    Create model User

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,16 @@
+const conn = require('../config/dbConnection');
 
+const User = {
+    login: async () => {
+        return await conn.query(`SELECT id, password, username, auth, level FROM \`user\` WHERE username = '${username}'`, (err, rows) => {
+            if (err) {
+                throw new Error(err);
+            }
+            if (rows) {
+                return rows;
+            }
+        })
+    }
+}
 
-
-
+module.exports = { User };
