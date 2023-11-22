0d211c76c06aa779e7865233f22d7e25dfc8e5c8
michele25-10 <michele.gabri01@gmail.com>
2023-11-22 21:01:14 +0100

    rotta di logout con model

--- models/user.model.js
+++ models/user.model.js
@@ -1,16 +1,17 @@
 const conn = require('../config/dbConnection');
 
+var value;
+
 const User = {
-    login: async () => {
-        return await conn.query(`SELECT id, password, username, auth, level FROM \`user\` WHERE username = '${username}'`, (err, rows) => {
+    login: async (username) => {
+        const result = await conn.query(`SELECT id, password, username, auth, level FROM \`user\` WHERE username = '${username}'`, (err, rows) => {
             if (err) {
                 throw new Error(err);
             }
-            if (rows) {
-                return rows;
-            }
+            value = rows;
         })
+        return value;
     }
 }
 
-module.exports = { User };
+module.exports = User;
--- routes/auth/auth.controller.js
+++ routes/auth/auth.controller.js
@@ -4,6 +4,7 @@ const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcrypt');
 const conn = require('../../config/dbConnection');
 const User = require('../../models/user.model');
+const constants = require('../../constants');
 
 //@desc accedere con un utente
 //@route POST /api/users/login
@@ -11,44 +12,27 @@ const User = require('../../models/user.model');
 loginUser = asyncHandler(async (req, res) => {
     const { username, password } = req.body;
 
-    const result = await User.login(username);
+    const objUser = await User.login(username);
 
-    console.log(result);
-
-    /*const mysql = "SELECT id, password, username, auth, level FROM `user` WHERE username = '" + username + "' AND status='0'; ";
-
-    await conn.query(mysql, function (err, rows) {
-        if (err) {
-            res.status(500);
-            throw new Error(err);
-        }
-        if (rows.length == 1) {
-            bcrypt.compare(password, rows[0].password, function (err, result) {
-                if (result == true) {
-                    const accessToken = jwt.sign({
-                        /*Payload incorporato all'interno del token */
-    /*user: {
-        id: rows[0].id,
-        username: rows[0].username,
-        auth: rows[0].auth,
-        level: rows[0].level
+    if (objUser) {
+        bcrypt.compare(password, objUser.password, (err, result) => {
+            if (result) {
+                const accessToken = jwt.sign({
+                    /*Payload incorporato all'interno del token */
+                    user: {
+                        id: rows[0].id,
+                        username: rows[0].username,
+                        auth: rows[0].auth,
+                        level: rows[0].level
+                    }
+                }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
+            } else {
+                res.status(constants.UNAUTHORIZED);
+            }
+        });
+    } else {
+        res.status(constants.NOT_FOUND);
     }
-},
-    process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: "1h" });
-res.status(200).json({ accessToken });
-} else {
-res.status(400);
-throw new Error("Password o email erratta");
-};
-});
-} else {
-res.status(500);
-throw new Error("Internal error");
-}
-});*/
-
-
 });
 
 module.exports = { loginUser };
