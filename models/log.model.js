dee6ada2dc8fa3ae0deb296e4084b00dd4c48e68
michele25-10 <michele.gabri01@gmail.com>
2023-11-24 20:54:04 +0100

    Imbastita setLog, da finire il model Query erratta, devo costruire la data in modo migliore o appoggiarmi a moment...

---
+++ models/log.model.js
@@ -0,0 +1,12 @@
+const query = require('../utils/query');
+
+const Log = {
+    setLog: async (ip_address, status, token, body, dataora = new Date()) => {
+        const mysql = `INSERT INTO log(ip_address, status, token, body, dataora) VALUES ('${ip_address}', ${status}, '${token}', '${body}', ${dataora.toString()})`;
+        const result = await query(mysql);
+        console.log(result);
+        return result;
+    }
+}
+
+module.exports = Log; 
--- routes/auth/auth.controller.js
+++ routes/auth/auth.controller.js
@@ -3,6 +3,7 @@ const asyncHandler = require('express-async-handler');
 const User = require('../../models/user.model');
 const constants = require('../../constants');
 const { hash } = require('../../utils/crypto');
+const Log = require('../../models/log.model');
 
 //@desc accedere con un utente
 //@route POST /api/users/login
@@ -15,7 +16,6 @@ loginUser = asyncHandler(async (req, res) => {
     const objUser = await User.login(username);
 
     if (objUser) {
-
         if (hashedPassword === objUser.password) {
             const accessToken = jwt.sign({
                 /*Payload incorporato all'interno del token */
@@ -26,13 +26,16 @@ loginUser = asyncHandler(async (req, res) => {
                     level: objUser.level
                 }
             }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
+            await Log.setLog(req.ip, true, accessToken, JSON.stringify(req.body));
             res.status(200).send({
                 accessToken: accessToken
             });
         } else {
+            await setLog(req.ip, false, null, JSON.stringify(req.body));
             res.status(constants.UNAUTHORIZED);
         }
     } else {
+        await setLog(req.ip, false, null, JSON.stringify(req.body));
         res.status(constants.NOT_FOUND);
     }
 });
