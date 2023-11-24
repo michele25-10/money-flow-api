650f3b43d8150efdbeb757064b77f2c18b005651
michele25-10 <michele.gabri01@gmail.com>
2023-11-24 20:57:12 +0100

    Terminata API di login

--- models/log.model.js
+++ models/log.model.js
@@ -1,8 +1,8 @@
 const query = require('../utils/query');
 
 const Log = {
-    setLog: async (ip_address, status, token, body, dataora = new Date()) => {
-        const mysql = `INSERT INTO log(ip_address, status, token, body, dataora) VALUES ('${ip_address}', ${status}, '${token}', '${body}', ${dataora.toString()})`;
+    setLog: async (ip_address, status, token, body, dataora = new Date().toISOString()) => {
+        const mysql = `INSERT INTO log(ip_address, status, token, body, dataora) VALUES ('${ip_address}', ${status}, '${token}', '${body}', '${dataora.toString()}')`;
         const result = await query(mysql);
         console.log(result);
         return result;
