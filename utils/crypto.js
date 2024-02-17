b9c110ea0f231c2577f3a1bca0ea55fb93512917
michele25-10 <michele.gabri01@gmail.com>
2024-02-17 18:48:25 +0100

    modificata utils crypto

--- utils/crypto.js
+++ utils/crypto.js
@@ -1,4 +1,4 @@
-const { createHash } = require('crypto');
+/*const { createHash } = require('crypto');
 const crypto = require('crypto');
 
 const hash = (stringa) => {
@@ -29,6 +29,39 @@ const decrypt = (encryptedData, secretKey) => {
     let decrypted = decipher.update(encryptedData.encryptedText, 'hex', 'utf8');
     decrypted += decipher.final('utf8');
     return decrypted;
+}
+
+module.exports = { hash, encrypt, decrypt };*/
+
+const CryptoJS = require('crypto-js');
+
+// Funzione per creare l'hash di una stringa utilizzando l'algoritmo SHA-256
+const hash = (stringa) => {
+    const result = CryptoJS.SHA256(stringa).toString();
+    return result;
+}
+
+// Funzione per generare la chiave segreta utilizzando l'algoritmo SHA-256
+const generateSecretKey = (secretKey) => {
+    return CryptoJS.SHA256(secretKey);
+}
+
+// Funzione per crittografare un testo utilizzando AES-256-CBC
+const encrypt = (text, secretKey) => {
+    const iv = CryptoJS.lib.WordArray.random(16);
+    secretKey = generateSecretKey(secretKey);
+    const encrypted = CryptoJS.AES.encrypt(text, secretKey, { iv: iv });
+    return {
+        iv: iv.toString(CryptoJS.enc.Hex),
+        encryptedText: encrypted.toString()
+    };
+}
+
+// Funzione per decrittografare un testo crittografato con AES-256-CBC
+const decrypt = (encryptedData, secretKey) => {
+    secretKey = generateSecretKey(secretKey);
+    const decrypted = CryptoJS.AES.decrypt(encryptedData.encryptedText, secretKey, { iv: CryptoJS.enc.Hex.parse(encryptedData.iv) });
+    return decrypted.toString(CryptoJS.enc.Utf8);
 }
 
 module.exports = { hash, encrypt, decrypt };
