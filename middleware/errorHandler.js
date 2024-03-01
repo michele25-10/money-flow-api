293f73733062da60ebf427af96e73bf8538571a2
michele25-10 <michele.gabri01@gmail.com>
2024-03-01 15:26:19 +0100

    Fix bug e sistemati piccoli dettagli per la produzione

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -4,22 +4,27 @@ const errorHandler = (err, req, res, next) => {
         res.statusCode = 500;
     }
     const statusCode = res.statusCode ? res.statusCode : 500;
+    let json;
     switch (statusCode) {
         case constants.VALIDATION_ERROR:
-            res.json({ title: "Validation Failed", message: err.message, stackTrace: err.stack });
+            json = { title: "Validation Failed", message: err.message, stackTrace: err.stack };
             break;
         case constants.NOT_FOUND:
-            res.json({ title: "Not Found", message: err.message, stackTrace: err.stack });
+            json = { title: "Not Found", message: err.message, stackTrace: err.stack };
             break;
         case constants.FORBIDDEN:
-            res.json({ title: "Forbidden", message: err.message, stackTrace: err.stack });
+            json = { title: "Forbidden", message: err.message, stackTrace: err.stack };
             break;
         case constants.UNAUTHORIZED:
-            res.json({ title: "Unauthorized", message: err.message, stackTrace: err.stack });
+            json = { title: "Unauthorized", message: err.message, stackTrace: err.stack };
             break;
         case constants.SERVER_ERROR:
-            res.json({ title: "Server error", message: err.message, stackTrace: err.stack });
+            json = { title: "Server error", message: err.message, stackTrace: err.stack };
             break;
     }
+    if (process.env.NODE_ENV === "production") {
+        delete (json.stackTrace);
+    }
+    res.send(json);
 };
 module.exports = errorHandler;
