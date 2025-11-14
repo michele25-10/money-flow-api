64965795a3ffeb741756f684f5ef7ca8efcc9057
Michele Gabrieli <michele.gabri01@gmail.com>
2025-11-14 12:34:56 +0100

    Modifiche con rotta health

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-require('express-async-errors');
+const express = require("express");
+const cors = require("cors");
+require("express-async-errors");
 const errorHandler = require("./middleware/errorHandler");
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Consenti solo questi metodi HTTP
-    allowedHeaders: ['Content-Type', 'Authorization'] // Consenti solo questi header
-}));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Consenti solo questi metodi HTTP
+    allowedHeaders: ["Content-Type", "Authorization"], // Consenti solo questi header
+  })
+);
 
 const port = process.env.PORT || 5000; //prendo la porta dal file .env
 
 //middleware
 app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
 
 app.use("/api", require("./routes/index.route"));
 
@@ -23,5 +29,5 @@ app.use("/api", require("./routes/index.route"));
 app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log('Server running on port ' + port);
+  console.log("Server running on port " + port);
 });
